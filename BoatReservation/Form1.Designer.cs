564a23b0582b557b4dddbdab345652efcf990532
Connor Dufault <mx8434an@go.minnstate.edu>
2020-02-06 11:40:18 -0600

    added checks for specific invalid boat rental cases and corresponding messages

--- BoatReservation/Form1.Designer.cs
+++ BoatReservation/Form1.Designer.cs
@@ -28,39 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.numAdults = new System.Windows.Forms.NumericUpDown();
-            this.numKids = new System.Windows.Forms.NumericUpDown();
             this.cmboBoatType = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.btnReserve = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cmboAdults = new System.Windows.Forms.ComboBox();
+            this.cmboKids = new System.Windows.Forms.ComboBox();
             this.btnCheckRes = new System.Windows.Forms.Button();
             this.lblCheckRes = new System.Windows.Forms.Label();
-            this.btnReserve = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numAdults)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numKids)).BeginInit();
             this.SuspendLayout();
-            // 
-            // numAdults
-            // 
-            this.numAdults.Location = new System.Drawing.Point(257, 94);
-            this.numAdults.Name = "numAdults";
-            this.numAdults.Size = new System.Drawing.Size(57, 20);
-            this.numAdults.TabIndex = 2;
-            // 
-            // numKids
-            // 
-            this.numKids.Location = new System.Drawing.Point(257, 147);
-            this.numKids.Name = "numKids";
-            this.numKids.Size = new System.Drawing.Size(57, 20);
-            this.numKids.TabIndex = 3;
             // 
             // cmboBoatType
             // 
             this.cmboBoatType.FormattingEnabled = true;
-            this.cmboBoatType.Location = new System.Drawing.Point(193, 40);
+            this.cmboBoatType.Location = new System.Drawing.Point(206, 40);
             this.cmboBoatType.Name = "cmboBoatType";
-            this.cmboBoatType.Size = new System.Drawing.Size(121, 21);
+            this.cmboBoatType.Size = new System.Drawing.Size(108, 21);
             this.cmboBoatType.TabIndex = 4;
             // 
             // label1
@@ -90,69 +76,110 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "How many children?";
             // 
-            // btnCheckRes
-            // 
-            this.btnCheckRes.Location = new System.Drawing.Point(50, 222);
-            this.btnCheckRes.Name = "btnCheckRes";
-            this.btnCheckRes.Size = new System.Drawing.Size(106, 23);
-            this.btnCheckRes.TabIndex = 8;
-            this.btnCheckRes.Text = "Check Reservation";
-            this.btnCheckRes.UseVisualStyleBackColor = true;
-            this.btnCheckRes.Click += new System.EventHandler(this.btnCheckRes_Click);
-            // 
-            // lblCheckRes
-            // 
-            this.lblCheckRes.Location = new System.Drawing.Point(193, 222);
-            this.lblCheckRes.Name = "lblCheckRes";
-            this.lblCheckRes.Size = new System.Drawing.Size(121, 23);
-            this.lblCheckRes.TabIndex = 9;
-            this.lblCheckRes.Text = "Not checked";
-            this.lblCheckRes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // btnReserve
             // 
+            this.btnReserve.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnReserve.Enabled = false;
-            this.btnReserve.Location = new System.Drawing.Point(50, 267);
+            this.btnReserve.Location = new System.Drawing.Point(51, 264);
             this.btnReserve.Name = "btnReserve";
             this.btnReserve.Size = new System.Drawing.Size(75, 23);
             this.btnReserve.TabIndex = 10;
             this.btnReserve.Text = "Reserve";
             this.btnReserve.UseVisualStyleBackColor = true;
             // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(51, 307);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(239, 306);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cmboAdults
+            // 
+            this.cmboAdults.FormattingEnabled = true;
+            this.cmboAdults.Location = new System.Drawing.Point(263, 93);
+            this.cmboAdults.Name = "cmboAdults";
+            this.cmboAdults.Size = new System.Drawing.Size(51, 21);
+            this.cmboAdults.TabIndex = 13;
+            // 
+            // cmboKids
+            // 
+            this.cmboKids.FormattingEnabled = true;
+            this.cmboKids.Location = new System.Drawing.Point(263, 146);
+            this.cmboKids.Name = "cmboKids";
+            this.cmboKids.Size = new System.Drawing.Size(51, 21);
+            this.cmboKids.TabIndex = 14;
+            // 
+            // btnCheckRes
+            // 
+            this.btnCheckRes.Location = new System.Drawing.Point(50, 208);
+            this.btnCheckRes.Name = "btnCheckRes";
+            this.btnCheckRes.Size = new System.Drawing.Size(108, 23);
+            this.btnCheckRes.TabIndex = 15;
+            this.btnCheckRes.Text = "Check Reservation";
+            this.btnCheckRes.UseVisualStyleBackColor = true;
+            this.btnCheckRes.Click += new System.EventHandler(this.btnCheckRes_Click);
+            // 
+            // lblCheckRes
+            // 
+            this.lblCheckRes.AutoSize = true;
+            this.lblCheckRes.Location = new System.Drawing.Point(215, 213);
+            this.lblCheckRes.Name = "lblCheckRes";
+            this.lblCheckRes.Size = new System.Drawing.Size(69, 13);
+            this.lblCheckRes.TabIndex = 16;
+            this.lblCheckRes.Text = "Not checked";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 336);
-            this.Controls.Add(this.btnReserve);
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(364, 350);
             this.Controls.Add(this.lblCheckRes);
             this.Controls.Add(this.btnCheckRes);
+            this.Controls.Add(this.cmboKids);
+            this.Controls.Add(this.cmboAdults);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnReserve);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cmboBoatType);
-            this.Controls.Add(this.numKids);
-            this.Controls.Add(this.numAdults);
             this.Name = "Form1";
             this.Text = "Rental Form";
-            ((System.ComponentModel.ISupportInitialize)(this.numAdults)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numKids)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.NumericUpDown numAdults;
-        private System.Windows.Forms.NumericUpDown numKids;
         private System.Windows.Forms.ComboBox cmboBoatType;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReserve;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cmboAdults;
+        private System.Windows.Forms.ComboBox cmboKids;
         private System.Windows.Forms.Button btnCheckRes;
         private System.Windows.Forms.Label lblCheckRes;
-        private System.Windows.Forms.Button btnReserve;
     }
 }
 
--- BoatReservation/Form1.cs
+++ BoatReservation/Form1.cs
@@ -15,12 +15,136 @@ namespace BoatReservation
         public Form1()
         {
             InitializeComponent();
-            cmboBoatType. = 
+
+            cmboBoatType.Items.Add("Canoe");
+            cmboBoatType.Items.Add("Kayak");
+            cmboBoatType.Items.Add("Paddleboard");
+
+            // I made these combo boxes, because values outside of these will never be valid
+            cmboAdults.Items.Add(1);
+            cmboAdults.Items.Add(2);
+            cmboKids.Items.Add(0);
+            cmboKids.Items.Add(1);
+
+            cmboAdults.SelectedItem = 1;
+            cmboKids.SelectedItem = 0;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
         private void btnCheckRes_Click(object sender, EventArgs e)
         {
+            if (cmboBoatType.SelectedItem != null) // ensure a boat type selection has been made
+            {
+                string boatType = cmboBoatType.SelectedItem.ToString();
+                int adults = (int)cmboAdults.SelectedItem;
+                int kids = (int)cmboKids.SelectedItem;
 
+                bool validRes = CheckRes(boatType, adults, kids);
+
+                if (validRes)
+                {
+                    lblCheckRes.Text = "Valid";
+                    btnReserve.Enabled = true;
+                }
+                else
+                {
+                    lblCheckRes.Text = "Invalid";
+                    btnReserve.Enabled = false;
+                }
+            }
+            else
+            {
+                lblCheckRes.Text = "Select a boat type";
+            }
+        }
+
+        private bool CheckRes(string boatType, int adults, int kids)
+        {
+            if (boatType == "Canoe")
+            {
+                return CheckCanoe(adults, kids);
+            }
+            else if (boatType == "Kayak")
+            {
+                return CheckKayak(adults, kids);
+            }
+            else if (boatType == "Paddleboard")
+            {
+                return CheckPaddleboard(adults, kids);
+            }    
+            else // false by default, in case of something weird
+            {    
+                return false;
+            }        
+        }
+
+        private bool CheckCanoe(int adults, int kids)
+        {
+            // valid cases
+            if ((adults == 2 && kids == 0) || (adults == 1 && kids == 1)) // 2 people, at least 1 must be adult
+            {
+                return true;
+            }
+
+            // invalid cases
+            else if (adults == 1 && kids == 0)
+            {
+                MessageBox.Show("Canoes require two people, at least one of whom must be an adult.", "Invalid");
+                return false;
+            }
+            else if (adults == 2 && kids == 1)
+            {
+                MessageBox.Show("Canoes cannot accomodate more than two people.", "Invalid");
+                return false;
+            }
+
+            else // default
+            {
+                return false;
+            }
+        }
+
+        private bool CheckKayak(int adults, int kids)
+        {
+            if (adults == 1 && kids == 0) // one adult only
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Kayaks can only accomodate one adult.", "Invalid");
+                return false;
+            }
+        }
+
+        private bool CheckPaddleboard(int adults, int kids)
+        {
+            if (adults == 1) // only 1 adult, may include 1 kid
+            {
+                return true;
+            }
+            else if (adults == 2)
+            {
+                MessageBox.Show("Paddleboards can only accomodate one adult.", "Invalid");
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cmboBoatType.SelectedItem = null;
+            cmboAdults.SelectedItem = 1;
+            cmboKids.SelectedItem = 0;
+
+            btnReserve.Enabled = false;
         }
     }
 }
